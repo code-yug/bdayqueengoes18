83ad8f0142546ea77c49ca38c74699566bca6861
not_YUG <111147972+code-yug@users.noreply.github.com>
2025-04-20 03:22:40 +0530

    Add files via upload

--- javaceleb.js
+++ javaceleb.js
@@ -1,99 +1,31 @@
-let W = window.innerWidth;
-let H = window.innerHeight;
-const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
-const maxConfettis = 150;
-const particles = [];
+const duration = 60 * 60 * 1000,
+	animationEnd = Date.now() + duration,
+	defaults = { startVelocity: 30, spread: 360, ticks: 20, zIndex: 0 };
 
-const possibleColors = [
-  "DodgerBlue",
-  "OliveDrab",
-  "Gold",
-  "Pink",
-  "SlateBlue",
-  "LightBlue",
-  "Gold",
-  "Violet",
-  "PaleGreen",
-  "SteelBlue",
-  "SandyBrown",
-  "Chocolate",
-  "Crimson"
-];
-
-function randomFromTo(from, to) {
-  return Math.floor(Math.random() * (to - from + 1) + from);
+function randomInRange(min, max) {
+	return Math.random() * (max - min) + min;
 }
 
-function confettiParticle() {
-  this.x = Math.random() * W; // x
-  this.y = Math.random() * H - H; // y
-  this.r = randomFromTo(11, 33); // radius
-  this.d = Math.random() * maxConfettis + 11;
-  this.color =
-    possibleColors[Math.floor(Math.random() * possibleColors.length)];
-  this.tilt = Math.floor(Math.random() * 33) - 11;
-  this.tiltAngleIncremental = Math.random() * 0.07 + 0.05;
-  this.tiltAngle = 0;
+const interval = setInterval(function () {
+	const timeLeft = animationEnd - Date.now();
 
-  this.draw = function() {
-    context.beginPath();
-    context.lineWidth = this.r / 2;
-    context.strokeStyle = this.color;
-    context.moveTo(this.x + this.tilt + this.r / 3, this.y);
-    context.lineTo(this.x + this.tilt, this.y + this.tilt + this.r / 5);
-    return context.stroke();
-  };
-}
+	if (timeLeft <= 0) {
+		return clearInterval(interval);
+	}
 
-function Draw() {
-  const results = [];
+	const particleCount = 20 * (timeLeft / duration);
 
-  requestAnimationFrame(Draw);
-
-  context.clearRect(0, 0, W, window.innerHeight);
-
-  for (var i = 0; i < maxConfettis; i++) {
-    results.push(particles[i].draw());
-  }
-
-  let particle = {};
-  let remainingFlakes = 0;
-  for (var i = 0; i < maxConfettis; i++) {
-    particle = particles[i];
-
-    particle.tiltAngle += particle.tiltAngleIncremental;
-    particle.y += (Math.cos(particle.d) + 3 + particle.r / 2) / 2;
-    particle.tilt = Math.sin(particle.tiltAngle - i / 3) * 15;
-
-    if (particle.y <= H) remainingFlakes++;
-
-    
-    if (particle.x > W + 30 || particle.x < -30 || particle.y > H) {
-      particle.x = Math.random() * W;
-      particle.y = -30;
-      particle.tilt = Math.floor(Math.random() * 10) - 20;
-    }
-  }
-
-  return results;
-}
-
-window.addEventListener(
-  "resize",
-  function() {
-    W = window.innerWidth;
-    H = window.innerHeight;
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-  },
-  false
-);
-
-for (var i = 0; i < maxConfettis; i++) {
-  particles.push(new confettiParticle());
-}
-
-canvas.width = W;
-canvas.height = H;
-Draw();
+	// since particles fall down, start a bit higher than random
+	confetti(
+		Object.assign({}, defaults, {
+			particleCount,
+			origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 }
+		})
+	);
+	confetti(
+		Object.assign({}, defaults, {
+			particleCount,
+			origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 }
+		})
+	);
+}, 250);
